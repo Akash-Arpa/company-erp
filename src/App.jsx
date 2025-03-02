f4a7d6d5f62a330ad21963d668b75512d92326d9
Shekhar-Sahu-FI <shekhar.sahu@arpaerp.com>
2025-03-02 15:45:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
-import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import PoMain from "./components/purchaseOrder/PoMain";
-import SearchPo from "./components/purchaseOrder/SearchPo";
+import './App.css'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PoMain />} />
-        <Route path="po-search" element={<SearchPo />} />
-      </Routes>
-    </Router>
-  );
+    <>
+     <Home/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
