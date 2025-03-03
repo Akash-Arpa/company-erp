46233652be289a6c21039b41c16d14f9229391db
Priya Tiwari <priya.tiwari@arpaerp.com>
2025-03-03 13:44:10 +0530

    adding report

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PoMain from "./components/purchaseOrder/PoMain";
 import SearchPo from "./components/purchaseOrder/SearchPo";
+import Report from "./components/report/Report";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PoMain />} />
         <Route path="po-search" element={<SearchPo />} />
+        <Route path="report" element={<Report />} />
       </Routes>
     </Router>
   );
