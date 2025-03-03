2f3a810223c4667caf15f26681845196646bf3b9
Shekhar-Sahu-FI <shekhar.sahu@arpaerp.com>
2025-03-03 13:18:07 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import PoMain from "../components/purchaseOrder/PoMain";
+import PO from "./purchaseOrder/PO";
+import GRN from "./grn/GRN";
+import Report from "./Report/Report";
+import MaterialIssue from "./materialIssue/materialIssue";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+//   const navigate = useNavigate();
+
+  const tab = [
+    {
+      tab: PO,
+      Name: "PurchaseOrder",
+      title: "po-main",
+    },
+    {
+      tab: GRN,
+      Name: "GRN",
+      title: "grn",
+    },
+    {
+      tab: Report,
+      Name: "Report",
+      title: "report",
+    },
+    {
+      tab: MaterialIssue,
+      Name: "Material Issue",
+      title: "material-issue",
+    },
+  ];
+
+  const handleActiveTab = (title) => {
+    setActiveIndex((prev) => {
+      return title;
+    });
+    //navigate(title);
+  };
+
+  const [activeIndex, setActiveIndex] = useState(1);
+  let ActiveTab = tab[activeIndex].tab;
+
+  return (
+    <>
+
+      <div style={{marginBottom:"20px"}}>
+        {tab.map((ele, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => {
+                // handleActiveTab(ele.title);
+                handleActiveTab(index);
+              }}
+              style={{ margin: "5px" }}
+            >
+              {ele.Name}
+            </button>
+          );
+        })}
+      </div>
+      <hr style={{marginBottom:"20px", width:"100%"}}/>
+      <div>
+        <ActiveTab />
+      </div>
+    </>
+  );
+}
