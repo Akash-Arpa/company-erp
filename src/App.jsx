452a6a765151cc9c7425116601e239f922596b8e
Shekhar-Sahu-FI <shekhar.sahu@arpaerp.com>
2025-03-03 13:20:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from "react";
-import "./App.css";
+import './App.css'
+import Home from './components/Home'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import PoMain from "./components/purchaseOrder/PoMain";
-import SearchPo from "./components/purchaseOrder/SearchPo";
+import PoMain from './components/purchaseOrder/PoMain';
+import SearchPo from './components/purchaseOrder/SearchPo';
+import GRN from './components/grn/GRN';
+import Report from './components/Report/Report';
+import MaterialIssue from './components/materialIssue/materialIssue';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  
   return (
-    <Router>
+    <>
+    <Home/>
+     {/* <Router>
       <Routes>
-        <Route path="/" element={<PoMain />} />
+        <Route path="/" element={ <Home/>} />
+        <Route path="/po-main" element={ <PoMain/>} />
         <Route path="po-search" element={<SearchPo />} />
+        <Route path="/grn" element={<GRN />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/material-issue" element={<MaterialIssue />} />
       </Routes>
-    </Router>
-  );
+    </Router> */}
+    
+    </>
+  )
 }
 
-export default App;
+export default App
+
