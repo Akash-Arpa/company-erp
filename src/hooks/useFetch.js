2a256ee82b395fac069768763c294dde74af25c3
Shekhar-Sahu-FI <shekhar.sahu@arpaerp.com>
2025-03-03 13:19:07 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,33 @@
-export async function useFetch(url) {
-  const response = await fetch(url);
-  // console.log(response);
-  const data = await response.json();
-  // console.log(data);
 
+import { useEffect, useState } from "react";
+
+const useFetch = async (url) => {
+
+  const response = await fetch(url);
+
+  const data = await response.json();
   return data.result;
+};
+
+export default useFetch;
+
+
+export function useFetchData(url) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const res = await response.json();
+        setData(res.result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return data;
 }
